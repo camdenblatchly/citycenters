db6043327790166640172405d0838230f2355b18
Camden Blatchly <camdenblatchly@gmail.com>
2025-07-09 12:15:40 -0400

    trying densest tract approach

--- scripts/utils.R
+++ scripts/utils.R
@@ -4,8 +4,6 @@ acs_vars <- c(
 
 get_pop_job_centers <- function(principle_geoid_st, geoid_pl) {
 
-  # principle_geoid_st <- top50_simple$PrincipleCityStateFIPS[i]
-
   acs_raw <- tidycensus::get_acs(
     "block group",
     variables = acs_vars,
@@ -64,5 +62,59 @@ get_pop_job_centers <- function(principle_geoid_st, geoid_pl) {
     )
 
   return(pop_weighted_center)
+
+}
+
+get_densest_tract <- function(principle_geoid_st, geoid_pl) {
+
+  acs_raw <- tidycensus::get_acs(
+    "tract",
+    variables = acs_vars,
+    year = 2019,
+    state = principle_geoid_st,
+    summary_var = "B01001_001"
+  ) %>%
+    select(-c(NAME, moe, summary_moe)) %>%
+    rename(
+      tot_pop = summary_est,
+      short_commute_pop = estimate
+    )
+
+  state_tracts <- tigris::tracts(state = principle_geoid_st, cb = TRUE, year = 2019)
+
+  place_geo <- tigris::places(state = principle_geoid_st) %>%
+    filter(GEOID == geoid_pl)
+
+  place_tracts <- state_tracts %>%
+    filter(st_intersects(., place_geo, sparse = FALSE) %>% apply(1, any))
+
+  place_dta <- left_join(
+    place_tracts,
+    acs_raw,
+    by = "GEOID"
+  ) %>%
+    # Remove water only tracts and ones without people
+    filter(ALAND > 0) %>%
+    # Minimum population size for block group
+    filter(tot_pop > 100) %>%
+    mutate(
+      pct_with_short_commute = short_commute_pop / tot_pop,
+      pop_density = tot_pop / ALAND
+    ) %>%
+    # Only bigger pop tracts
+    filter(tot_pop >= quantile(tot_pop, .2, na.rm = TRUE)) %>%
+    # calculate Block Group Lat Lon
+    mutate(
+      centroid = st_centroid(geometry)
+    ) %>%
+    mutate(
+      lon = st_coordinates(centroid)[, 1],
+      lat = st_coordinates(centroid)[, 2]
+    ) %>%
+    st_as_sf() %>%
+    st_transform(crs = 4326) %>%
+    slice_max(order_by = pop_density, n = 1)
+
+  return(place_dta)
 
 }
